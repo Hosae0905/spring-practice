27355d209c4e21e84b36dc7a3fb69baec3f32e3a
hosin <didghtls11@gmail.com>
2024-12-12 14:17:59 +0900

    [ADD] 스프링 핵심 원리 이해2 - 객체 지향 원리 적용 공부

--- spring-basic/src/main/java/project/springbasic/AppConfig.java
+++ spring-basic/src/main/java/project/springbasic/AppConfig.java
@@ -1,5 +1,7 @@
 package project.springbasic;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import project.springbasic.discount.DiscountPolicy;
 import project.springbasic.discount.FixDiscountPolicy;
 import project.springbasic.discount.RateDiscountPolicy;
@@ -10,6 +12,10 @@ import project.springbasic.member.MemoryMemberRepository;
 import project.springbasic.order.OrderService;
 import project.springbasic.order.OrderServiceImpl;
 
+/**
+ * @Configuration은 애플리케이션의 설정 정보를 담은 클래스를 의미한다.
+ */
+@Configuration
 public class AppConfig {
 
     /**
@@ -20,16 +26,31 @@ public class AppConfig {
 //        return new MemberServiceImpl(new MemoryMemberRepository());
 //    }
 
+    /**
+     * 스프링 적용하기 전
+     * @return MemberServiceImpl 객체
+     */
+//    public MemberService memberService() {
+//        return new MemberServiceImpl(memberRepository());
+//    }
+
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
     /**
+     * 스프링 적용하기 전
      * MemberRepository 역할이 드러나게 바꿔준다. --> 이를 통해서 역할과 구현 클래스(MemoryMemberRepository)가 한 눈에 들어온다.
      * 역할을 드러냈기 때문에 추후 메모리에서 DB로 변경될 시 해당 코드만 변경하면 된다.
      * @return MemberRepository
      */
-    private MemberRepository memberRepository() {
+//    private MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
+
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
@@ -41,15 +62,31 @@ public class AppConfig {
 //        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
 //    }
 
+    /**
+     * 스프링 적용하기 전
+     * @return OrderServiceImpl 객체
+     */
+//    public OrderService orderService() {
+//        return new OrderServiceImpl(memberRepository(), discountPolicy());
+//    }
+
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
     /**
+     * 스프링 적용하기 전
      * DiscountPolicy 역할이 드러나게 바꿔준다. --> 이를 통해서 역할과 구현 클래스(FixDiscountPolicy)가 한 눈에 들어온다.
      * 할인 정책이 변경되면 해당 코드만 변경하면 된다.
      * @return DiscountPolicy
      */
+//    public DiscountPolicy discountPolicy() {
+////        return new FixDiscountPolicy();
+//        return new RateDiscountPolicy();        // 새로운 할인 정책을 적용하려면 해당 코드만 변경하면 된다.
+//    }
+
+    @Bean
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();        // 새로운 할인 정책을 적용하려면 해당 코드만 변경하면 된다.
--- spring-basic/src/main/java/project/springbasic/MemberApp.java
+++ spring-basic/src/main/java/project/springbasic/MemberApp.java
@@ -1,5 +1,7 @@
 package project.springbasic;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import project.springbasic.member.Grade;
 import project.springbasic.member.Member;
 import project.springbasic.member.MemberService;
@@ -18,8 +20,27 @@ public class MemberApp {
         /**
          * AppConfig 클래스를 통한 의존성 주입 후 예제를 실행
          */
-        AppConfig appConfig = new AppConfig();
+        /*AppConfig appConfig = new AppConfig();
         MemberService memberService = appConfig.memberService();
+        Member memberA = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(memberA);
+
+        Member member = memberService.findMember(1L);
+        System.out.println("member.getName() = " + member.getName());
+        System.out.println("memberA.getName() = " + memberA.getName());*/
+
+        /**
+         * 스프링 적용 후
+         */
+
+        /*
+        * AnnotationConfigApplicationContext의 파라미터로 설정 정보가 담긴 AppConfig 클래스를 넣으면
+        * @Bean이 붙은 것들을 모두 생성하여 스프링 컨테이너에 넣어준다.
+        * */
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // 기본적으로 메서드 이름으로 등록된다. --> 자바의 리플렉션에 대해서 알아보기
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
         Member memberA = new Member(1L, "memberA", Grade.VIP);
         memberService.join(memberA);
 
