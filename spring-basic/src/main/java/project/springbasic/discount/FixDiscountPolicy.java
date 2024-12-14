15960bf6ce27a550e99cb24986f2f484229d456d
hosin <didghtls11@gmail.com>
2024-12-14 15:50:55 +0900

    [Feat] 의존관계 자동 주입 공부

--- spring-basic/src/main/java/project/springbasic/discount/FixDiscountPolicy.java
+++ spring-basic/src/main/java/project/springbasic/discount/FixDiscountPolicy.java
@@ -1,8 +1,12 @@
 package project.springbasic.discount;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 import project.springbasic.member.Grade;
 import project.springbasic.member.Member;
 
+@Component      // 같은 DiscountPolicy를 빈으로 등록하게 되면 발생하는 문제
+//@Qualifier("fixDiscountPolicy")
 public class FixDiscountPolicy implements DiscountPolicy {
 
     private int discountFixAmount = 1000;
